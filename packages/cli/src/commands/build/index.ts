678e3795665e96e0297f0a2ba9f4cc8502200519
aacebo <aacebowork@gmail.com>
2024-12-24 23:16:20 +0800

    stuff

--- packages/cli/src/commands/build/index.ts
+++ packages/cli/src/commands/build/index.ts
@@ -1,10 +1,46 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+import * as tsup from 'tsup';
 import { CommandModule } from 'yargs';
 
-export const build: CommandModule = {
-  command: 'build',
+interface Args {
+  readonly path: string;
+}
+
+export const build: CommandModule<{ }, Args> = {
+  command: 'build <path>',
   aliases: 'b',
   describe: 'build a project',
-  handler: async () => {
+  builder: (b) => {
+    return b.positional('path', {
+      alias: 'p',
+      type: 'string',
+      describe: 'path to the project directory',
+      demandOption: true
+    });
+  },
+  handler: async (args) => {
+    const srcDir = path.join(args.path, 'src');
+    const outDir = path.join(args.path, 'dist');
 
+    await tsup.build({
+      bundle: true,
+      dts: true,
+      minify: true,
+      sourcemap: true,
+      treeshake: true,
+      splitting: true,
+      clean: true,
+      platform: 'browser',
+      target: 'es2015',
+      outDir,
+      entry: [path.join(srcDir, 'index.ts')]
+    });
+
+    fs.copyFileSync(
+      path.join(srcDir, 'index.html'),
+      path.join(outDir, 'index.html')
+    );
   }
 };
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -6,9 +6,7 @@ import { hideBin } from 'yargs/helpers';
 import * as commands from './commands';
 
 (async () => {
-  const argv = await yargs(hideBin(process.argv))
+  await yargs(hideBin(process.argv))
     .command(commands.build)
     .parse();
-
-  console.log(argv);
 })();
